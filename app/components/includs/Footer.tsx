5b94d3166a9ecc1c591e44a8a5b744eb5512de3b
Md.Ali Rabbi <56753742+RABBI31@users.noreply.github.com>
2023-06-22 00:37:05 +0600

    add-footers-in page

---
+++ app/components/includs/Footer.tsx
@@ -0,0 +1,16 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="bg-gray-900 text-white py-4">
+                <div className="container mx-auto text-center">
+                    <p className="text-sm">
+                        &copy; 2023 Your Website. All rights reserved. | Powered by FeedAHelp
+                    </p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
--- app/pages/Homepage.tsx
+++ app/pages/Homepage.tsx
@@ -1,16 +1,21 @@
 import { getAllTodos } from "@/api"
 import AddTask from "../components/AddTask"
 import TodoList from "../components/TodoList"
+import Footer from "../components/includs/Footer";
 const HomePage = async () => {
   const tasks = await getAllTodos();
   return (
-   <div className=" mt-5 max-w-4xl mx-auto">
+    <div className="flex flex-col min-h-screen">
+   <main className="flex-grow mt-5 max-w-6xl mx-auto">
      <h1 className="text-xl my-5 flex flex-col gap-4 bold uppercase text-center">Todo List</h1>
      <div className="text-center">
      <AddTask />
      </div>
-    
      <TodoList tasks={tasks}/>
+   </main>
+    <footer>
+    <Footer />
+   </footer>
    </div>
   )
 }
